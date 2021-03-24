80beea04785301c47e2a417ec9b83047a1b774ff
joferava <jfelipe221095@gmail.com>
2021-03-24 14:34:03 -0600

    Initial Commit

---
+++ cognito/cognitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cognitoApp.swift
+//  cognito
+//
+//  Created by Felipe Ramirez Vargas on 24/3/21.
+//
+
+import SwiftUI
+
+@main
+struct cognitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
