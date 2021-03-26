32619d0e93f8efc416957d08ea309c1f368f3785
joferava <jfelipe221095@gmail.com>
2021-03-25 18:57:24 -0600

    First Commit

--- cognito/ContentView.swift
+++ cognito/ContentView.swift
@@ -6,11 +6,77 @@
 //
 
 import SwiftUI
+import Amplify
+
+class ContentViewMoedl: ObservableObject {
+    @Published var logged = false
+    
+    func isLogged()->Bool{
+        return logged
+    }
+}
 
 struct ContentView: View {
+    @StateObject var contentVM = ContentViewMoedl()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            if contentVM.isLogged() {
+                Text("Logged")
+                    .toolbar {
+                        Button("LogOut"){
+                            self.logOut()
+                        }
+                    }
+            }else {
+                VStack{
+                    NavigationLink(destination: SignUp()){
+                        HStack(alignment: .center){
+                            Spacer()
+                            Text("SignUp").foregroundColor(Color.white)
+                            Spacer()
+                        }
+                    }.padding().background(Color.green).cornerRadius(15.0)
+                    NavigationLink(destination: LoginView()) {
+                        HStack(alignment: .center){
+                            Spacer()
+                            Text("LogIn").foregroundColor(Color.white)
+                            Spacer()
+                        }
+                    }.padding()
+                    .background(Color.blue)
+                    .cornerRadius(15.0)
+                }.padding()
+            }
+        }
+        .onAppear{self.fetchCurrentAuthSession()}
+        .navigationBarTitle("Welcome")
+        .environmentObject(self.contentVM)
+    }
+    func fetchCurrentAuthSession(){
+        Amplify.Auth.fetchAuthSession { result in
+            switch result {
+            case.success(let session):
+                print("Is user signed in - \(session.isSignedIn)")
+                if session.isSignedIn {
+                        self.contentVM.logged = true
+                }
+            case.failure(let error):
+                print("Fetch session failed with error \(error)")
+            }
+        }
+    }
+    
+    func logOut(){
+        Amplify.Auth.signOut(){ result in
+            switch result {
+            case.success:
+                print("Successfully signed out")
+                self.contentVM.logged = false
+            case.failure(let error):
+                print("Sign out failed with error \(error)")
+                self.contentVM.logged = true
+            }
+        }
     }
 }
 
--- cognito/cognitoApp.swift
+++ cognito/cognitoApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import Amplify
+import AmplifyPlugins
 
 @main
 struct cognitoApp: App {
+    init() {
+        configureAmplify()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    private func configureAmplify(){
+        do{
+            Amplify.Logging.logLevel = .verbose
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.configure()
+              print("Amplify OK")
+              print("Amplify configured with auth plugin")
+        } catch {
+            print("An error ocurred setting up Amplify: \(error)")
         }
     }
 }
